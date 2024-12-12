594af933dad62e760bd6f38a33d5ca65d9d04246
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2024-12-12 19:27:42 +0900

    feat: H-2 Task added

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,17 @@
+/* H-2 Task */
+
+/* Shunday function tuzing, unga string argument pass bolsin. Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+MASALAN: getDigits("m14i1t") return qiladi "141" */
+
+const getDigits = (text: string) => {
+  const newGetDigits = text.split('').filter((char) => !isNaN(Number(char))).join('');
+  return newGetDigits;
+}; 
+
+console.log(getDigits("m14i1t"));
+
+console.log(getDigits("hb828b0d2"));
+
 /* H Task */
 
 /* Raqamlardan iborat arrayni argument sifatida qabul qiladigan
@@ -5,15 +19,15 @@ function tuzing. Ushbu function faqatgina positive sonlarni olib
 string holatida return qilsin.
 MASALAN: getPositive([1, -4, 2]) return qiladi "12". */
 
-function getPositive(arr: number[]): string {
-    let argument = "";
+// function getPositive(arr: number[]): string {
+//     let argument = "";
   
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > 0) {
-        argument += arr[i];
-      }
-    }
-    return argument;
-  }
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] > 0) {
+//         argument += arr[i];
+//       }
+//     }
+//     return argument;
+//   }
   
-  console.log(getPositive([2, 3, -12, -21, 8])); 
+//   console.log(getPositive([2, 3, -12, -21, 8])); 
