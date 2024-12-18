18160f557979c0d854396ddf10e402acfe0b59b9
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2024-12-19 04:28:29 +0900

    fix: modify restaurant login and signup processes (bcrypt hash)

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/errors";
 import { MemberType } from "../libs/enum/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
     private readonly memberModel;
@@ -10,12 +11,14 @@ class MemberService {
         this.memberModel = MemberModel;
     }
 
-
     public async processSignup(input: MemberInput): Promise<Member> {
         const exist = await this.memberModel
             .findOne({memberType: MemberType.RESTAURANT})
             .exec();
         if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
              
         try {
             const result = await this.memberModel.create(input);
@@ -24,7 +27,6 @@ class MemberService {
         } catch (err) {
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
-        
     }
 
     public async processLogin(input: LoginInput): Promise<Member> {
@@ -34,12 +36,13 @@ class MemberService {
                 { memberNick: 1, memberPassword: 1 }
             )
             .exec();
+        if(!member) throw new Errors(HttpCode.NOT_FOUND,  Message.NO_MEMBER_NICK);
 
-        if(!member) {
-            throw new Errors(HttpCode.NOT_FOUND,  Message.NO_MEMBER_NICK);
-    }
+        const isMatch = await bcrypt.compare(
+            input.memberPassword, 
+            member.memberPassword
+        );
 
-        const isMatch = input.memberPassword === member.memberPassword;
         if(!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
         }
