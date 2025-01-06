b2b403f2b9932808a08e421b21ed829b7344e684
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2025-01-07 05:05:23 +0900

    feat: modify image preloading process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,29 @@
 console.log("Signup frontend javascript file");
 
-$(function() {});
+$(function() {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change", function() {
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if (!validImageType.includes(fileType)) {
+                alert("Please insert only jpeg, jpg, png!")
+            } else {
+                if (uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                        .attr("src", URL.createObjectURL(uploadFile))
+                        .addClass("success");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename);
+        }
+    });
+});
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -14,12 +37,19 @@ function validateSignupForm() {
         memberPassword === "" || 
         confirmPassword === ""
     ) {
-        alert("Please insert all reuired inputs!");
+        alert("Please insert all required inputs!");
         return false;
     }
 
     if (memberPassword !== confirmPassword) {
         alert("Passwrod differs, please check!");
+        return false;
+    }
+
+    const memberImage = $(".member-image").get(0).files[0].name 
+    ? $(".member-image").get(0).files[0].name : null;
+    if (!memberImage) {
+        alert("Please insert restaurant image!");
         return false;
     }
 }
