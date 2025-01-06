ec6c9641d3d10e017af9c7a8291a70b704aedc10
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2025-01-07 04:26:30 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,25 @@
 console.log("Signup frontend javascript file");
+
+$(function() {});
+
+function validateSignupForm() {
+    const memberNick = $(".member-nick").val();
+    const memberPhone = $(".member-phone").val();
+    const memberPassword = $(".member-password").val();
+    const confirmPassword = $(".confirm-password").val();
+    
+    if (
+        memberNick === "" || 
+        memberPhone === "" || 
+        memberPassword === "" || 
+        confirmPassword === ""
+    ) {
+        alert("Please insert all reuired inputs!");
+        return false;
+    }
+
+    if (memberPassword !== confirmPassword) {
+        alert("Passwrod differs, please check!");
+        return false;
+    }
+}
