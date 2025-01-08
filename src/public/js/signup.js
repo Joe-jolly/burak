9bc943a07db17cca7656a31a758a05c3dffa914a
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2025-01-09 02:22:11 +0900

    fix: modify products page based on products data

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,29 +1,27 @@
 console.log("Signup frontend javascript file");
 
-$(function() {
+$(function () {
     const fileTarget = $(".file-box .upload-hidden");
-    let filename;
-
-    fileTarget.on("change", function() {
-        if (window.FileReader) {
-            const uploadFile = $(this)[0].files[0];
-            const fileType = uploadFile["type"];
-            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
-            if (!validImageType.includes(fileType)) {
-                alert("Please insert only jpeg, jpg, png!")
-            } else {
-                if (uploadFile) {
-                    console.log(URL.createObjectURL(uploadFile));
-                    $(".upload-img-frame")
-                        .attr("src", URL.createObjectURL(uploadFile))
-                        .addClass("success");
-                }
-                filename = $(this)[0].files[0].name;
-            }
-            $(this).siblings(".upload-name").val(filename);
+  
+    fileTarget.on("change", function () {
+      if (window.FileReader) {
+        const uploadFile = $(this)[0].files[0];
+        const fileType = uploadFile["type"];
+        const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+        if (!validImageType.includes(fileType)) {
+          alert("Please insert only jpeg, jpg and png!");
+        } else if (uploadFile) {
+          let filename;
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+          filename = $(this)[0].files[0].name;
+          $(this).siblings(".upload-name").val(filename);
         }
+      }
     });
-});
+  });
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -46,7 +44,7 @@ function validateSignupForm() {
         return false;
     }
 
-    const memberImage = $(".member-image").get(0).files[0].name 
+    const memberImage = $(".member-image").get(0)?.files[0]?.name 
     ? $(".member-image").get(0).files[0].name : null;
     if (!memberImage) {
         alert("Please insert restaurant image!");
