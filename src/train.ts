5f3fcf3aba5ee9a7df1cc7cee57c770fa6356638
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2025-03-08 16:12:38 +0900

    ZL task added

--- src/train.ts
+++ src/train.ts
@@ -24,25 +24,44 @@
   Backend validation;
   Database valiadtion
 */
+/* ###########################    ###############################   ######################*/
+/** ZL-TASK: */
+/** Shunday function yozing, u parametrda berilgan stringni kebab casega otkazib qaytarsin. Bosh harflarni kichik harflarga ham otkazsin.
+MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab” */
+
+function stringToKebab(str: string): string {
+  return str.trim()
+    .toLowerCase()
+    .replace(/[^a-z0-9\s]/g, '') // Maxsus belgilarni olib tashlaydi
+    .replace(/\s+/g, '-'); // Bo'sh joylarni '-' bilan almashtiradi
+}
+console.log(stringToKebab("I love Kebab"));
+console.log(stringToKebab("Hello @World! This is #JavaScript"));  
+
+
+
+
+
+
 
 /* ###########################    ###############################   ######################*/
 /** ZK-TASK: */
 /**Shunday function yozing, u har soniyada bir marta consolega 1 dan 5 gacha bolgan raqamlarni chop etsin va 5 soniyadan keyin ishini toxtatsin.
 MASALAN: printNumbers() */
 
-async function printNumbers() {
-  let second = 0;
-  const interval = setInterval(() => {
-    second++;
-    console.log(second, second === 1 ? 'second' : 'seconds');
-  }, 1000);
+// async function printNumbers() {
+//   let second = 0;
+//   const interval = setInterval(() => {
+//     second++;
+//     console.log(second, second === 1 ? 'second' : 'seconds');
+//   }, 1000);
   
-  await new Promise(resolve => setTimeout(resolve, 6000));
+//   await new Promise(resolve => setTimeout(resolve, 6000));
   
-  clearInterval(interval);
-  console.log('Done! The counting process is finished!')
-}
-printNumbers();
+//   clearInterval(interval);
+//   console.log('Done! The counting process is finished!')
+// }
+// printNumbers();
 
 
 
